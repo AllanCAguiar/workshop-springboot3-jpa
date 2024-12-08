009765c61be4deb368d455513bdd7dd3a31240d6
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2024-12-08 16:56:20 -0400

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -54,6 +54,14 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public Double getTotal() {
+        double sum = 0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     public Long getId() {
         return this.id;
     }
@@ -95,7 +103,6 @@ public class Order implements Serializable {
     public void setPayment(Payment payment) {
         this.payment = payment;
     }
-
 
     public Set<OrderItem> getItems() {
         return items;
--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -32,6 +32,10 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    public Double getSubTotal() {
+        return price * quantity;
+    }
+
     @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
@@ -64,6 +68,8 @@ public class OrderItem implements Serializable {
     public void setPrice(Double price) {
         this.price = price;
     }
+
+    
 
     @Override
     public int hashCode() {
